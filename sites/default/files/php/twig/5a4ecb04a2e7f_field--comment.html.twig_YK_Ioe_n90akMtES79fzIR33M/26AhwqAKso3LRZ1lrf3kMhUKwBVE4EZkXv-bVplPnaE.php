5c79b2986b8f00cceb1cf8162795d7b146a45b66
joginder <jogind3r@gmail.com>
2018-01-05 07:11:57 +0530

    drupal

---
+++ sites/default/files/php/twig/5a4ecb04a2e7f_field--comment.html.twig_YK_Ioe_n90akMtES79fzIR33M/26AhwqAKso3LRZ1lrf3kMhUKwBVE4EZkXv-bVplPnaE.php
@@ -0,0 +1,126 @@
+<?php
+
+/* core/themes/classy/templates/field/field--comment.html.twig */
+class __TwigTemplate_dabccf888bb49590f99614516242e2ea4b632f7d76783109e167bc42c7eac0d0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 29, "if" => 44);
+        $filters = array("clean_class" => 31, "t" => 53);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set', 'if'),
+                array('clean_class', 't'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 29
+        $context["classes"] = array(0 => "field", 1 => ("field--name-" . \Drupal\Component\Utility\Html::getClass(        // line 31
+(isset($context["field_name"]) ? $context["field_name"] : null))), 2 => ("field--type-" . \Drupal\Component\Utility\Html::getClass(        // line 32
+(isset($context["field_type"]) ? $context["field_type"] : null))), 3 => ("field--label-" .         // line 33
+(isset($context["label_display"]) ? $context["label_display"] : null)), 4 => "comment-wrapper");
+        // line 38
+        $context["title_classes"] = array(0 => "title", 1 => (((        // line 40
+(isset($context["label_display"]) ? $context["label_display"] : null) == "visually_hidden")) ? ("visually-hidden") : ("")));
+        // line 43
+        echo "<section";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "html", null, true));
+        echo ">
+  ";
+        // line 44
+        if (((isset($context["comments"]) ? $context["comments"] : null) &&  !(isset($context["label_hidden"]) ? $context["label_hidden"] : null))) {
+            // line 45
+            echo "    ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["title_prefix"]) ? $context["title_prefix"] : null), "html", null, true));
+            echo "
+    <h2";
+            // line 46
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["title_attributes"]) ? $context["title_attributes"] : null), "addClass", array(0 => (isset($context["title_classes"]) ? $context["title_classes"] : null)), "method"), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["label"]) ? $context["label"] : null), "html", null, true));
+            echo "</h2>
+    ";
+            // line 47
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["title_suffix"]) ? $context["title_suffix"] : null), "html", null, true));
+            echo "
+  ";
+        }
+        // line 49
+        echo "
+  ";
+        // line 50
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["comments"]) ? $context["comments"] : null), "html", null, true));
+        echo "
+
+  ";
+        // line 52
+        if ((isset($context["comment_form"]) ? $context["comment_form"] : null)) {
+            // line 53
+            echo "    <h2 class=\"title comment-form__title\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Add new comment")));
+            echo "</h2>
+    ";
+            // line 54
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["comment_form"]) ? $context["comment_form"] : null), "html", null, true));
+            echo "
+  ";
+        }
+        // line 56
+        echo "
+</section>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/field/field--comment.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  94 => 56,  89 => 54,  84 => 53,  82 => 52,  77 => 50,  74 => 49,  69 => 47,  63 => 46,  58 => 45,  56 => 44,  51 => 43,  49 => 40,  48 => 38,  46 => 33,  45 => 32,  44 => 31,  43 => 29,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/themes/classy/templates/field/field--comment.html.twig", "/private/var/www/drupaltest/core/themes/classy/templates/field/field--comment.html.twig");
+    }
+}
---
+++ sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupaltest',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'fBiqhs22yEdbJZblzpMv4YwRSCtCza61qPp2NWjStM7zAw_0AfMAFcvipl0IYvr6T17qVXe8WQ';
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_-aDCKI3xAbGDZl465VKdBlqJ7KrWFd1H7o66aStyl4JoC609RPJ33aOiXDQP7mFINCLjSHM-kw/sync';
